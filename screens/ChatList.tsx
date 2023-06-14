cbd299a7ee8a77843abd7ea58e8c2f7d3ec18ecc
Joseph Ulyatt <joeulyatt1@hotmail.co.uk>
2023-06-14 10:42:30 +0200

    remove redundant useState code from ChatList related to HandleSignIn function

--- screens/ChatList.tsx
+++ screens/ChatList.tsx
@@ -1,39 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import {
-	Text,
-	TouchableOpacity,
-} from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import {
 	updateProfile,
 	signInAnonymously,
 	onAuthStateChanged,
 } from 'firebase/auth';
-import { auth } from '../firebase/firebase'
+import { auth } from '../firebase/firebase';
 import {
 	uniqueNamesGenerator,
 	adjectives,
 	animals,
 } from 'unique-names-generator';
-import { faker } from '@faker-js/faker';
 import { StackNavigator } from '../components/Navigation/Types';
 
 type ChatListScreenProps = NativeStackScreenProps<StackNavigator, 'ChatList'>;
 
 const ChatList = ({ navigation }: ChatListScreenProps) => {
-	const [name, setName] = useState('');
-	const [email, setEmail] = useState(faker.internet.email());
-	const [password, setPassword] = useState(faker.internet.password());
-	const [avatar, setAvatar] = useState('');
-
-	const shortName = uniqueNamesGenerator({
-		dictionaries: [adjectives, animals],
-		length: 2,
-	});
-
-
-
 	const handleSignInGuest = () => {
 		signInAnonymously(auth)
 			.then(() => {
@@ -45,18 +29,12 @@ const ChatList = ({ navigation }: ChatListScreenProps) => {
 								dictionaries: [adjectives, animals],
 								length: 2,
 							}),
-							photoURL: avatar
-								? avatar
-								: 'https://gravatar.com/avatar/94d45dbdba988afacf30d916e7aaad69?s=200&d=mp&r=x',
+							photoURL:
+								'https://gravatar.com/avatar/94d45dbdba988afacf30d916e7aaad69?s=200&d=mp&r=x',
 						});
 						console.log(user);
-						// User is signed in, see docs for a list of available properties
-						// https://firebase.google.com/docs/reference/js/auth.user
-						const uid = user.uid;
-						// ...
 					} else {
 						// User is signed out
-						// ...
 					}
 				});
 			})
