735d08fd76f80ea24ab86ca17b56cccc0020982f
Joseph Ulyatt <joeulyatt1@hotmail.co.uk>
2023-06-22 11:44:20 +0200

    move singIn from App.tsx to separate component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { NativeBaseProvider, extendTheme } from 'native-base';
 
-import { updateProfile, signInAnonymously, onAuthStateChanged } from 'firebase/auth';
-import { auth } from './firebase/firebase';
-
-import { uniqueNamesGenerator, adjectives, animals } from 'unique-names-generator';
 import { config } from './theme';
 import Navigation from './components/Navigation/index';
+import signIn from './components/SignIn/Index';
 
 import { SSRProvider } from '@react-aria/ssr';
 import { useFonts } from 'expo-font';
@@ -14,39 +11,16 @@ import { useFonts } from 'expo-font';
 const theme = extendTheme(config);
 
 type MyThemeType = typeof theme;
-declare module 'native-base' { interface ICustomTheme extends MyThemeType {} };
+declare module 'native-base' {
+	interface ICustomTheme extends MyThemeType {}
+}
 
 export default function App() {
 	const [fontsLoaded] = useFonts({
 		'Jua-Regular': require('./assets/fonts/Jua-Regular.ttf'),
 	});
 
-	async function getImg() {
-		const response = await fetch('https://loremflickr.com/json/320/240');
-		const jsonData = await response.json();
-		return jsonData.file;
-	}
-
-	useEffect(() => {
-		signInAnonymously(auth)
-			.then(getImg)
-			.then(res => {
-				onAuthStateChanged(auth, user => {
-					if (user) {
-						updateProfile(user, {
-							displayName: uniqueNamesGenerator({
-								dictionaries: [adjectives, animals],
-								length: 2,
-							}),
-							photoURL: res,
-						});
-					}
-				});
-			})
-			.catch(error => {
-				console.log(error);
-			});
-	}, []);
+	signIn();
 
 	if (!fontsLoaded) {
 		return null;
