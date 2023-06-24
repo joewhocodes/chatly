607c34abbed85e0ee6491df49523fd1039e3b050
Joseph Ulyatt <joeulyatt1@hotmail.co.uk>
2023-06-24 09:37:59 +0200

    add unit tesitng for ChatHeader

--- components/ChatHeader/ChatHeader.test.tsx
+++ components/ChatHeader/ChatHeader.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
 import renderer from 'react-test-renderer';
-import { NativeBaseProvider } from 'native-base';
+import { NativeBaseProvider, Heading, Text } from 'native-base';
 
 import ChatHeader from './index';
 
@@ -9,12 +10,67 @@ describe('ChatHeader', () => {
     const chatName = 'Test Chat';
     const setShowModal = jest.fn();
 
-    const tree = renderer.create(
-      <NativeBaseProvider>
-        <ChatHeader chatName={chatName} setShowModal={setShowModal} navigation={undefined} />
-      </NativeBaseProvider>
-    ).toJSON();
+    const tree = renderer
+      .create(
+        <NativeBaseProvider>
+          <ChatHeader chatName={chatName} setShowModal={setShowModal} navigation={undefined} />
+        </NativeBaseProvider>
+      )
+      .toJSON();
 
     expect(tree).toMatchSnapshot();
+  });
+
+  it('calls handleBackPress when back arrow is pressed', () => {
+    const chatName = 'Test Chat';
+    const setShowModal = jest.fn();
+    const navigation = {
+      navigate: jest.fn(),
+    };
+
+    const component = renderer.create(
+      <NativeBaseProvider>
+        <ChatHeader chatName={chatName} setShowModal={setShowModal} navigation={navigation} />
+      </NativeBaseProvider>
+    );
+
+    const backArrow = component.root.findByProps({ testID: 'back-arrow' });
+    backArrow.props.onPress();
+
+    expect(navigation.navigate).toHaveBeenCalledWith('ChatList');
+  });
+
+  it('calls handleChangeName when "Tap to change name" text is pressed', () => {
+    const chatName = 'Test Chat';
+    const setShowModal = jest.fn();
+    const navigation = undefined;
+
+    const component = renderer.create(
+      <NativeBaseProvider>
+        <ChatHeader chatName={chatName} setShowModal={setShowModal} navigation={navigation} />
+      </NativeBaseProvider>
+    );
+
+    const changeNameText = component.root.findByProps({ testID: 'change-name-text' });
+    changeNameText.props.onPress();
+
+    expect(setShowModal).toHaveBeenCalledWith(true);
+  });
+
+  it('displays the chat name correctly', () => {
+    const chatName = 'Test Chat';
+    const setShowModal = jest.fn();
+    const navigation = undefined;
+
+    const component = renderer.create(
+      <NativeBaseProvider>
+        <ChatHeader chatName={chatName} setShowModal={setShowModal} navigation={navigation} />
+      </NativeBaseProvider>
+    );
+
+    const heading = component.root.findByProps({ testID: 'chat-name-heading' });
+    const renderedText = heading.props.children;
+
+    expect(renderedText).toBe(chatName);
   });
 });
