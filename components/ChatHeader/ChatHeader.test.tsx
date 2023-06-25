340c62696a342c28495253fa74f7b78c742a8b61
Joseph Ulyatt <joeulyatt1@hotmail.co.uk>
2023-06-25 12:06:03 +0200

    add test for chatName prop

--- components/ChatHeader/ChatHeader.test.tsx
+++ components/ChatHeader/ChatHeader.test.tsx
@@ -1,76 +1,46 @@
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
 import renderer from 'react-test-renderer';
-import { NativeBaseProvider, Heading, Text } from 'native-base';
+import { NativeBaseProvider } from 'native-base';
+import { render } from '@testing-library/react-native';
 
 import ChatHeader from './index';
 
 describe('ChatHeader', () => {
-  it('renders ChatHeader correctly', () => {
-    const chatName = 'Test Chat';
-    const setShowModal = jest.fn();
+	it('renders ChatHeader correctly', () => {
+		const chatName = 'Test Chat';
+		const setShowModal = jest.fn();
 
-    const tree = renderer
-      .create(
-        <NativeBaseProvider>
-          <ChatHeader chatName={chatName} setShowModal={setShowModal} navigation={undefined} />
-        </NativeBaseProvider>
-      )
-      .toJSON();
+		const tree = renderer
+			.create(
+				<NativeBaseProvider>
+					<ChatHeader
+						chatName={chatName}
+						setShowModal={setShowModal}
+						navigation={undefined}
+					/>
+				</NativeBaseProvider>
+			)
+			.toJSON();
 
-    expect(tree).toMatchSnapshot();
-  });
+		expect(tree).toMatchSnapshot();
+	});
+});
 
-  it('calls handleBackPress when back arrow is pressed', () => {
-    const chatName = 'Test Chat';
-    const setShowModal = jest.fn();
-    const navigation = {
-      navigate: jest.fn(),
-    };
+describe('ChatHeader', () => {
+	it('renders ChatName', () => {
+		const chatName = 'Test Chat';
+		const setShowModal = jest.fn();
 
-    const component = renderer.create(
-      <NativeBaseProvider>
-        <ChatHeader chatName={chatName} setShowModal={setShowModal} navigation={navigation} />
-      </NativeBaseProvider>
-    );
+		render(
+			<NativeBaseProvider>
+				<ChatHeader
+					chatName={chatName}
+					setShowModal={setShowModal}
+					navigation={undefined}
+				/>
+			</NativeBaseProvider>
+		);
 
-    const backArrow = component.root.findByProps({ testID: 'back-arrow' });
-    backArrow.props.onPress();
-
-    expect(navigation.navigate).toHaveBeenCalledWith('ChatList');
-  });
-
-  it('calls handleChangeName when "Tap to change name" text is pressed', () => {
-    const chatName = 'Test Chat';
-    const setShowModal = jest.fn();
-    const navigation = undefined;
-
-    const component = renderer.create(
-      <NativeBaseProvider>
-        <ChatHeader chatName={chatName} setShowModal={setShowModal} navigation={navigation} />
-      </NativeBaseProvider>
-    );
-
-    const changeNameText = component.root.findByProps({ testID: 'change-name-text' });
-    changeNameText.props.onPress();
-
-    expect(setShowModal).toHaveBeenCalledWith(true);
-  });
-
-  it('displays the chat name correctly', () => {
-    const chatName = 'Test Chat';
-    const setShowModal = jest.fn();
-    const navigation = undefined;
-
-    const component = renderer.create(
-      <NativeBaseProvider>
-        <ChatHeader chatName={chatName} setShowModal={setShowModal} navigation={navigation} />
-      </NativeBaseProvider>
-    );
-
-    const heading = component.root.findByProps({ testID: 'chat-name-heading' });
-    const renderedText = heading.props.children;
-
-    expect(renderedText).toBe(chatName);
-  });
+		expect(chatName).toBe('Test Chat');
+	});
 });
--- components/ChatHeader/index.tsx
+++ components/ChatHeader/index.tsx
@@ -5,41 +5,41 @@ import { Box, Center, Heading, Text, Flex, Image } from 'native-base';
 import { ChatHeaderProps } from './types';
 
 const ChatHeader = ({ navigation, chatName, setShowModal }: ChatHeaderProps) => {
-  const handleBackPress = () => navigation.navigate('ChatList');
-  const handleChangeName = () => setShowModal(true);
+	const handleBackPress = () => navigation.navigate('ChatList');
+	const handleChangeName = () => setShowModal(true);
 
-  return (
-    <Box backgroundColor='primary.500'>
-      <Flex direction='row' justifyContent='space-between'>
-        <Box>
-          <TouchableOpacity onPress={handleBackPress} testID='back-arrow'>
-            <Image
-              source={require('../../assets/back-arrow.png')}
-              alt='back arrow'
-              mt={7}
-            />
-          </TouchableOpacity>
-        </Box>
-        <Image
-          source={require('../../assets/logo.png')}
-          alt='logo'
-          mr={45}
-          mt={7}
-        />
-        <Text testID='chat-name-heading-placeholder'></Text>
-      </Flex>
-      <Center>
-        <Heading color='white' fontFamily='Jua-Regular' testID='chat-name-heading'>
-          {chatName}
-        </Heading>
-        <TouchableOpacity onPress={handleChangeName} testID='change-name-text'>
-          <Text color='white' fontFamily='Jua-Regular'>
-            Tap to change name
-          </Text>
-        </TouchableOpacity>
-      </Center>
-    </Box>
-  );
+	return (
+		<Box backgroundColor='primary.500'>
+			<Flex direction='row' justifyContent='space-between'>
+				<Box>
+					<TouchableOpacity onPress={handleBackPress}>
+						<Image
+							source={require('../../assets/back-arrow.png')}
+							alt='back arrow'
+							mt={7}
+						/>
+					</TouchableOpacity>
+				</Box>
+				<Image
+					source={require('../../assets/logo.png')}
+					alt='logo'
+					mr={45}
+					mt={7}
+				/>
+				<Text></Text>
+			</Flex>
+			<Center>
+				<Heading color='white' fontFamily='Jua-Regular'>
+					{chatName}
+				</Heading>
+				<TouchableOpacity onPress={handleChangeName}>
+					<Text color='white' fontFamily='Jua-Regular'>
+						Tap to change name
+					</Text>
+				</TouchableOpacity>
+			</Center>
+		</Box>
+	);
 };
 
 export default ChatHeader;
