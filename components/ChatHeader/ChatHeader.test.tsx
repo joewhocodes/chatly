bb53750b9d23768290dab041624e1da2b7ec8974
Joe Ulyatt <joewhocodes@gmail.com>
2023-06-29 10:59:16 +0200

    add opens modal to testing in ChatHeader

--- components/ChatHeader/ChatHeader.test.tsx
+++ components/ChatHeader/ChatHeader.test.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { NativeBaseProvider } from 'native-base';
-import { render } from '@testing-library/react-native';
-
+import { fireEvent, render, screen } from '@testing-library/react-native';
 import ChatHeader from './index';
 
 describe('ChatHeader', () => {
@@ -42,5 +41,25 @@ describe('ChatHeader', () => {
 		);
 
 		expect(chatName).toBe('Test Chat');
+	});
+});
+
+describe('ChatHeader', () => {
+	it('opens modal', () => {
+		const chatName = 'Test Chat';
+		const setShowModal = jest.fn();
+
+		render(
+			<NativeBaseProvider>
+				<ChatHeader
+					chatName={chatName}
+					setShowModal={setShowModal}
+					navigation={undefined}
+				/>
+			</NativeBaseProvider>
+		);
+		
+		setShowModal(true);
+		expect(setShowModal).toBeCalledTimes(1);
 	});
 });
--- components/SwipeableItem/SwipeableItem.test.tsx
+++ components/SwipeableItem/SwipeableItem.test.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { NativeBaseProvider } from 'native-base';
-import { render } from '@testing-library/react-native';
 
 import SwipeableItem from './index';
 
 jest.mock('react-native-gesture-handler', () => {
-    return {
-      Swipeable: jest.fn(),
-    };
-  });
-  
+	return {
+		Swipeable: jest.fn(),
+	};
+});
+
 describe('SwipeableItem', () => {
 	it('renders SwipeableItem correctly', () => {
 		const item = {
-            chatName: 'Test Name',
-            chatId: 'Test Id',
-            messages: [],
-        }
+			chatName: 'Test Name',
+			chatId: 'Test Id',
+			messages: [],
+		};
 		const onDelete = jest.fn();
 		const onPress = jest.fn();
 
@@ -25,9 +24,9 @@ describe('SwipeableItem', () => {
 			.create(
 				<NativeBaseProvider>
 					<SwipeableItem
-                        item={item}
-                        onDelete={onDelete}
-                        onPress={onPress}
+						item={item}
+						onDelete={onDelete}
+						onPress={onPress}
 					/>
 				</NativeBaseProvider>
 			)
